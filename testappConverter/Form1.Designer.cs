3478befe6a2a309b80344af573f05c797816dd50
Jim Jones <wpa69469@hotmail.com.au>
2019-04-11 18:15:07 +1000

    test app changes

--- testappConverter/Form1.Designer.cs
+++ testappConverter/Form1.Designer.cs
@@ -28,32 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.fileDialog = new System.Windows.Forms.OpenFileDialog();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Browse";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
             // 
             // textBox1
             // 
@@ -69,7 +52,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 13);
+            this.button2.Location = new System.Drawing.Point(12, 13);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -77,38 +60,70 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox2
+            // button1
             // 
-            this.textBox2.Location = new System.Drawing.Point(217, 15);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(571, 20);
-            this.textBox2.TabIndex = 4;
+            this.button1.Location = new System.Drawing.Point(93, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(794, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(795, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1015, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.OpenFileDialog fileDialog;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- testappConverter/Form1.cs
+++ testappConverter/Form1.cs
@@ -7,38 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace testappConverter
 {
     public partial class Form1 : Form
     {
+        string filename;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                try
-                {
-                    label1.Text = fileDialog.FileName;
-                    string filePath = "@" + fileDialog.FileName;
-                    Bitmap bitmap = new Bitmap(filePath);
-                    for (int i = 1; i < bitmap.Width; i++)
-                    {
-                        for (int y = 1; y < bitmap.Height; y++)
-                        {
-                            textBox1.Text += bitmap.GetPixel(i, y).R + "," + bitmap.GetPixel(i, y).G + "," + bitmap.GetPixel(i, y).B + ",";
-                        }
-                    }
-                }
-                catch (System.Exception error)
-                {
-                    Console.WriteLine("Exception: " + error);
-                }
-            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -46,14 +25,19 @@ namespace testappConverter
             try
             {
                 textBox1.Text = "";
-                textBox2.Text = textBox2.Text.Replace(@"\", @"/");
-                Bitmap bitmap = new Bitmap(textBox2.Text);
-                textBox1.Text += "private Bitmap generated = new Bitmap(" + bitmap.Width + "," + bitmap.Height + "," + "new byte[] {";
+                filename = filename.Replace(@"\", @"/");
+                Bitmap bitmap = new Bitmap(filename);
+                FileInfo infoFile = new FileInfo(filename);
+                textBox1.Text += "private Bitmap " + infoFile.Name.Replace(infoFile.Extension, "") + " = new Bitmap(" + bitmap.Width + "," + bitmap.Height + "," + "new byte[] {";
                 for (int i = 0; i < bitmap.Width; i++)
                 {
                     for (int y = 0; y < bitmap.Height; y++)
                     {
-                        textBox1.Text += bitmap.GetPixel(i, y).A + "," + bitmap.GetPixel(i, y).R + "," + bitmap.GetPixel(i, y).G + "," + bitmap.GetPixel(i, y).B + ",";
+                        float alpha = bitmap.GetPixel(i, y).A;
+                        float R = bitmap.GetPixel(i, y).R;
+                        float G = bitmap.GetPixel(i, y).G;
+                        float B = bitmap.GetPixel(i, y).B;
+                        textBox1.Text += Convert.ToInt32(alpha) + "," + Convert.ToInt32(R) + "," + Convert.ToInt32(G) + "," + Convert.ToInt32(B) + ",";
                     }
                 }
                 textBox1.Text += "}, ColorDepth.ColorDepth32);";
@@ -61,6 +45,22 @@ namespace testappConverter
             catch (Exception eventerror)
             {
                 Console.WriteLine("Exception: " + eventerror);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Browse for the file
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                //set the file name
+                filename = dialog.FileName;
+                label1.Text = dialog.FileName;
+                FileInfo file = new FileInfo(dialog.FileName);
+                label2.Text = file.Name;
+                Bitmap bitmap = new Bitmap(filename);
+                pictureBox1.Image = bitmap;
             }
         }
     }
